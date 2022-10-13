03ec23af66d9483a92d07aa8164fda6b00a9420c
roy <Roy Shell>
2022-10-13 18:19:48 +0300

    Added empty message for properties page

--- src/components/pages/Properties/Properties.tsx
+++ src/components/pages/Properties/Properties.tsx
@@ -1,31 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import AssetListBlock from '../../elements/AssetListBlock/AssetListBlock';
+import './styles.scss';
 
 const PropertiesPage = (): JSX.Element => {
+
     const [allAssets, setAllAssets] = useState([]);
 
     useEffect(() => {
-  
         setAllAssets(getAllAssets());
     },[allAssets]);
+
+    const getEmptyMessage = (): JSX.Element => (
+        <div>Your properties will appear here</div>
+    );
+
 
     const getAllAssets = () => {
         return JSON.parse(localStorage.getItem('assets') || '[]');
     };
+
     return (
         <>
             <h1>My Properties</h1>
             <div className='sd-page-container'>
-             {
-             allAssets.map(({address, cost, description, image}, index) => 
-                (<AssetListBlock 
-                    address={address} 
-                    cost={cost} 
-                    description={description} 
-                    image={image} 
-                    key={index}
-                />
-             ))}
+                {
+                !!allAssets.length ? allAssets.map(({address, cost, description, image}, index) => 
+                    (<AssetListBlock 
+                        address={address} 
+                        cost={cost} 
+                        description={description} 
+                        image={image} 
+                        key={index}
+                    />
+                ))
+                : 
+                getEmptyMessage()
+                }
             </div>
         </>
     )
